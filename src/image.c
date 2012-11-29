91c8d406207cf1eb0e1c241b5d61a1a36fea8c7c
LaetitiaNanni <laetitia.nanni@gmail.com>
2012-11-29 10:42:37 +0100

    corrections makefiles pour compilation du programme

--- src/image.c
+++ src/image.c
@@ -1,2 +1,19 @@
 //ficher servant à déclarer les fonctions qui feront des opérations sur les images
 //il faut aussi creer un .h 
+//Structure IMAGE
+
+typedef struct structure_image 
+	{
+	// Numéro de variante
+	char numVar;
+	
+	// Hauteur et largeur de l'image
+	unsigned int hauteurImg;
+	unsigned int largeurImg;
+	
+	// Valeur maximale
+	int valMax;
+	
+	// Valeur des pixels
+	unsigned char* tabPixel; 
+	}str_Img;
