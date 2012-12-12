ce85b471dcc3eeb85e927f6e2fdb91dc499344d6
LaetitiaNanni <laetitia.nanni@gmail.com>
2012-12-12 14:03:04 +0100

    install.sh :commande de base du sample mise en commentaire

--- lib/install.sh
+++ lib/install.sh
@@ -6,7 +6,7 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR_LIB/.
 export LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 
-echo "============== COMPILING SAMPLE =================="
-cd ../
-make
-cd obj
+#echo "============== COMPILING SAMPLE =================="
+#cd ../
+#make
+#cd obj
