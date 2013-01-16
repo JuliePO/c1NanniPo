bcadd6fc005625cd8e3a9d2476f0a6124dca0e32
LaetitiaNanni <laetitia.nanni@gmail.com>
2013-01-16 23:57:57 +0100

    lut.h : correction bonne version

--- src/lut.h
+++ src/lut.h
@@ -3,17 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "image.h"
+#include "calque.h"
+
 /************* Structure du LUT *************/
 typedef struct LUT {
+
+	//numero de la lut
+	int id;
+
 	// Définition de 3 tableaux pour chacune des composantes 
 	// dont es valeurs vont de 0 à 255
 	unsigned char tabLutR[256];
 	unsigned char tabLutV[256];
 	unsigned char tabLutB[256];
 
-	//Calques 
-	struct LUT *l_prev; //calque précédent
-	struct LUT *l_next; //calque suivant
+	//pointeurs sur les Lut de la chaine
+	struct LUT *l_prev; //lut précédent
+	struct LUT *l_next; //lut suivant
 
 }LUT;
 
@@ -30,22 +37,23 @@ typedef struct LLUT {
 }LLUT;
 
 /************* Appel des fonctions *************/
-
-//
-int initLUT(LUT*);
-void applyLUT(LUT*, Image*);
-void delLUT(LUT*);
+LLUT* new_LLUT(void);
+int initLUT(LLUT*, LUT*);
+void applyLUT(Image*, LUT*);
+LLUT* removeLut(LLUT*, int);
 
 ////effets sur l'images
 
 //effets sur le contrast
-void addCon(LUT*, int);
-void dimCon(LUT*, int);
+void addCon(LLUT*, int);
+void dimCon(LLUT*, int);
 //effets sur la luminosité
-void addLum(LUT*, int);
-void dimLum(LUT*, int);
+void addLum(LLUT*, int);
+void dimLum(LLUT*, int);
 //effets sur les couleurs
-void invert(LUT*);
-void sepia(LUT*, int);
+void invert(LLUT*);
+void B_W(Image*);
+void colorize(LLUT*, int, int, int);
+void sepia (LUT*, Image*);
 
 #endif
