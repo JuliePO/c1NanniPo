de444bef7c4e0eb947636e40cbde170ace05a276
LaetitiaNanni <laetitia.nanni@gmail.com>
2013-01-16 23:32:02 +0100

    lut : fonction de gestion des listes

--- src/lut.c
+++ src/lut.c
@@ -1,42 +1,210 @@
 #include <stdlib.h>
 #include <stdio.h>
 //Appel de la structure d'une lut
-
+/*
 #include "lut.h"
 #include "image.h"
-#include "calque.h"
 
-/*
+*/
 //test en local
 #include "../include/lut.h"
 #include "../include/image.h"
 #include "../include/calque.h"
-*/
+
+
+
+/************* Création d'une nouvelle liste de lut *************/
+LLUT* new_LLUT(void) {
+	
+	//Alloue de la mémoire 
+	LLUT *p_lut = malloc(sizeof *p_lut);
+	if (p_lut != NULL) {
+		p_lut->length = 0;
+		p_lut->l_head = NULL;
+		p_lut->l_tail = NULL;
+	}
+	return p_lut;
+}
+
+/*********************************************************************/
+
+
 
 //initialisation de la lut
-int initLUT(LUT* lut) {
+int initLUT(LLUT* p_llut, LUT* new_lut) {
 
-	unsigned int i; // valeur entre 0-255
+	if (p_llut != NULL)	{
 
-	for (i=0; i<256; i++ ) {
-			lut->tabLutR[i] = i;
-			lut->tabLutV[i] = i;
-			lut->tabLutB[i] = i;
+		if (new_lut != NULL) {
+
+			//initialisaton des parametres
+
+			unsigned int i; // valeur entre 0-255
+
+			for (i=0; i<256; i++ ) {
+					new_lut->tabLutR[i] = i;
+					new_lut->tabLutV[i] = i;
+					new_lut->tabLutB[i] = i;
+			} 
+
+
+			//ajout a liste
+
+			// Cas où notre liste est vide (pointeur vers fin de liste à  NULL) et id = 0
+			if (p_llut->l_tail == NULL) {
+				new_lut->id = 0;
+
+				new_lut->l_prev = NULL; 
+				 // Pointe la tête de la liste sur le nouveau lut
+				p_llut->l_head = new_lut;
+				// Pointe la fin de la liste sur le nouveau lut
+				p_llut->l_tail = new_lut; 
+			}
+			// Cas où des éléments sont déjà présents dans la  liste
+			else {
+				new_lut->id = (p_llut->l_tail->id) + 1;
+				
+				// Relie le dernier lut de la liste au nouveau lut
+				p_llut->l_tail->l_next = new_lut; 
+				// Pointe p_prev du nouveau lut sur le dernier lut de la liste
+				new_lut->l_prev = p_llut->l_tail; 
+				// Pointe la fin de la liste sur le nouveau lut
+				p_llut->l_tail = new_lut; 
+			}
+			// On augmente de 1 la taille de la liste
+			p_llut->length++; 
+
+		}
+		else {
+			printf("OOps..cette lut n'existe pas \n");
+			return 0;
+		}
+
 	}
-
-	return 0;
+	else {
+		printf("OOps... Cette liste de lut n'existe pas \n");
+		return 0;
+	}
+	
+	//on retourne la liste
+	return 1;
 }	
+
+
+
+
+
+
 
 //application d'une LUT sur l'image
 // il s'agit de remplacer les valeurs des pixels de l'image par celles de la lut
-void remplirTab(Calque* calque, LUT* lut) {
+void applyLUT(Image* img, LUT* lut) {
 	int i;
-	for(i=0; i<((calque->image_src->heightImg)*(calque->image_src->widthImg)*3); i+=3){
-		calque->image_src->tabPixel[i]=(unsigned char)(lut->tabLutR[calque->image_src->tabPixel[i]]);
-		calque->image_src->tabPixel[i+1]=(unsigned char)(lut->tabLutV[calque->image_src->tabPixel[i+1]]);
-		calque->image_src->tabPixel[i+2]=(unsigned char)(lut->tabLutB[calque->image_src->tabPixel[i+2]]);
+	for(i=0; i<((img->heightImg)*(img->widthImg)*3); i+=3){
+		img->tabPixel[i]=(unsigned char)(lut->tabLutR[(int)img->tabPixel[i]]);
+		img->tabPixel[i+1]=(unsigned char)(lut->tabLutV[(int)img->tabPixel[i+1]]);
+		img->tabPixel[i+2]=(unsigned char)(lut->tabLutB[(int)img->tabPixel[i+2]]);
 	}
 }
+
+
+
+/************* Supprimer une lut selon sa position *************/
+LLUT* removeLut(LLUT* p_llut, int position) {
+
+	// On vérifie si notre liste a été allouée
+	if (p_llut != NULL) {
+
+		//Verifie que que le lut existe
+		if(p_llut->length >= position + 1) {
+
+			//Vérifie s'il reste au moins un lut, si c'est le dernier lut alors ne le supprime pas
+			if(p_llut->length > 1) {
+
+				//Création d'un lut temporaire
+				LUT *p_tmp = p_llut->l_head;
+
+				// Parcours de la liste de lut, tant que id est inférieur à la position souhaitée
+				while (p_tmp != NULL && (p_tmp->id) <= position) {
+
+					if (position == (p_tmp->id)) {
+				
+						//Si c'est le dernier lut de la liste
+						if (p_tmp->l_next == NULL) {
+					
+							//Pointe la fin de la liste sur le lut précédent
+							p_llut->l_tail = p_tmp->l_prev;
+							//Supprime le dernier lut ou lien du dernier lut vers le lut suivant est NULL
+							p_llut->l_tail->l_next = NULL;
+						}
+			
+						//Si c'est le premier lut de la liste
+						else if (p_tmp->l_prev == NULL) {
+							//Pointe la tête de la liste vers le lut suivant
+							p_llut->l_head = p_tmp->l_next;
+							//Le lien vers du deuxième lut vers le lut précédent est NULL ou Supprime le premier lut
+		 					p_llut->l_head->l_prev = NULL;
+							p_llut->l_head->id = 0;
+						
+							//Création d'un lut temporaire pour parcourir la liste de lut
+							LUT *p_temp = p_llut->l_head->l_next;
+							int i = 0;
+
+							// Parcours de la liste de lut, tant que i est inférieur à la position souhaitée
+							while (p_temp != NULL) {
+								p_temp->id = ++i;
+								p_temp = p_temp->l_next;
+							}
+							//Libère espace mémoire
+							free(p_temp);
+						}
+
+						else {
+							//Relie la lut suivant au lut précédent de la lut que l'on veut supprmer 
+							p_tmp->l_next->l_prev = p_tmp->l_prev;
+							//Relie la lut précédent au lut suivant de la lut que l'on veut supprmer 
+							p_tmp->l_prev->l_next = p_tmp->l_next;
+
+							//Création d'une lut temporaire pour parcourir la liste de lut à partir de la lut suivante du lut que l'on veut supprimer
+							LUT *p_temp = p_tmp->l_next;
+							int i = p_tmp->l_prev->id;
+
+							// Parcours de la liste de lut, tant que i est inférieur à la position souhaitée
+							while (p_temp != NULL) {
+								p_temp->id = ++i;
+								p_temp = p_temp->l_next;
+							}
+							//Libère espace mémoire
+							free(p_temp);
+
+						}
+						//Libère espace mémoire
+						free(p_tmp);
+						//Décrémente de un la taille de la liste
+						p_llut->length--;
+
+						break;
+			    		}
+			   		 else {
+						p_tmp = p_tmp->l_next;
+					}
+				}
+			}else
+				printf("Erreur : impossible de supprimer la lut\n");
+		}else
+			printf("Oops! Cette lut n'existe pas\n");
+	}
+
+	// on retourne notre nouvelle liste
+	return p_llut; 
+}
+
+
+
+
+
+
+
 
 /******Effets**********/
 
@@ -45,59 +213,79 @@ void remplirTab(Calque* calque, LUT* lut) {
 //il s'agit d'ajouté une valeur positive choisie aux valeurs des pixels
 //pour gagner en luminosité
 
-void addLum(LUT* lut, int nb){
-	int i;
- 
- 	for(i=0;i<256;i++) {
- 		if((lut->tabLutR[i]+nb)<0)
- 			lut->tabLutR[i] = 0;
- 		else if((lut->tabLutR[i]+nb)>255)
- 			lut->tabLutR[i] = 255;
- 		else
- 			lut->tabLutR[i] = lut->tabLutR[i]+nb;
- 			
- 		if((lut->tabLutV[i]+nb)<0)
- 			lut->tabLutV[i] = 0;
- 		else if((lut->tabLutV[i]+nb)>255)
- 			lut->tabLutV[i] = 255;
- 		else
- 			lut->tabLutV[i] = lut->tabLutV[i]+nb;
- 			
- 		if((lut->tabLutB[i]+nb)<0)
- 			lut->tabLutB[i] = 0;
- 		else if((lut->tabLutB[i]+nb)>255)
- 			lut->tabLutB[i] = 255;
- 		else
-   			lut->tabLutB[i] = lut->tabLutB[i]+nb;
+void addLum(LLUT* p_llut, int nb){
+
+	LUT *new_lut = malloc(sizeof *new_lut);
+
+	if (new_lut != NULL) {
+		
+		initLUT(p_llut,new_lut);
+
+		int i;
+	 	for(i=0;i<256;i++) {
+	 		if((new_lut->tabLutR[i]+nb)<0)
+	 			new_lut->tabLutR[i] = 0;
+	 		else if((new_lut->tabLutR[i]+nb)>255)
+	 			new_lut->tabLutR[i] = 255;
+	 		else
+	 			new_lut->tabLutR[i] = new_lut->tabLutR[i]+nb;
+	 			
+	 		if((new_lut->tabLutV[i]+nb)<0)
+	 			new_lut->tabLutV[i] = 0;
+	 		else if((new_lut->tabLutV[i]+nb)>255)
+	 			new_lut->tabLutV[i] = 255;
+	 		else
+	 			new_lut->tabLutV[i] = new_lut->tabLutV[i]+nb;
+	 			
+	 		if((new_lut->tabLutB[i]+nb)<0)
+	 			new_lut->tabLutB[i] = 0;
+	 		else if((new_lut->tabLutB[i]+nb)>255)
+	 			new_lut->tabLutB[i] = 255;
+	 		else
+	   			new_lut->tabLutB[i] = new_lut->tabLutB[i]+nb;
+	   }
+	}else {
+		printf("Erreur d'allocation de la lut \n");
 	}
+
 }
 
 //il s'agit d'ajouté une valeur négative choisie aux valeurs des pixels 
 //pour diminuer la luminosité
-void dimLum(LUT* lut, int nb){
-	int i;
+void dimLum(LLUT* p_llut, int nb){
+
+	LUT *new_lut = malloc(sizeof *new_lut);
+
+	if (new_lut != NULL) {
+		
+		initLUT(p_llut,new_lut);
+
+		int i;
  
- 	for(i=0;i<256;i++) {
- 		if((lut->tabLutR[i]-nb)<0)
- 			lut->tabLutR[i] = 0;
- 		else if((lut->tabLutR[i]-nb)>255)
- 			lut->tabLutR[i] = 255;
- 		else
- 			lut->tabLutR[i] = lut->tabLutR[i]-nb;
- 			
- 		if((lut->tabLutV[i]-nb)<0)
- 			lut->tabLutV[i] = 0;
- 		else if((lut->tabLutV[i]-nb)>255)
- 			lut->tabLutV[i] = 255;
- 		else
- 			lut->tabLutV[i] = lut->tabLutV[i]-nb;
- 			
- 		if((lut->tabLutB[i]-nb)<0)
- 			lut->tabLutB[i] = 0;
- 		else if((lut->tabLutB[i]-nb)>255)
- 			lut->tabLutB[i] = 255;
- 		else
-   			lut->tabLutB[i] = lut->tabLutB[i]-nb;
+	 	for(i=0;i<256;i++) {
+	 		if((new_lut->tabLutR[i]-nb)<0)
+	 			new_lut->tabLutR[i] = 0;
+	 		else if((new_lut->tabLutR[i]-nb)>255)
+	 			new_lut->tabLutR[i] = 255;
+	 		else
+	 			new_lut->tabLutR[i] = new_lut->tabLutR[i]-nb;
+	 			
+	 		if((new_lut->tabLutV[i]-nb)<0)
+	 			new_lut->tabLutV[i] = 0;
+	 		else if((new_lut->tabLutV[i]-nb)>255)
+	 			new_lut->tabLutV[i] = 255;
+	 		else
+	 			new_lut->tabLutV[i] = new_lut->tabLutV[i]-nb;
+	 			
+	 		if((new_lut->tabLutB[i]-nb)<0)
+	 			new_lut->tabLutB[i] = 0;
+	 		else if((new_lut->tabLutB[i]-nb)>255)
+	 			new_lut->tabLutB[i] = 255;
+	 		else
+	   			new_lut->tabLutB[i] = new_lut->tabLutB[i]-nb;
+		}   
+	}else {
+		printf("Erreur d'allocation de la lut \n");
 	}
 }
 
@@ -111,111 +299,136 @@ void dimLum(LUT* lut, int nb){
 
 
 
-void addContraste(LUT* lut, int nb) {
-	int i;
-	
-	for(i=0;i<256;i++) {
+void addContraste(LLUT* p_llut, int nb) {
+	LUT *new_lut = malloc(sizeof *new_lut);
 
-		if(lut->tabLutR[i] < 128) {
-			// couche des rouges
-			if((lut->tabLutR[i]-nb)>128)
-				lut->tabLutR[i] = 128;
-			else
-				lut->tabLutR[i] = lut->tabLutR[i]-nb;
-			
-			// couche des verts
-			if((lut->tabLutV[i]-nb)>128)
-				lut->tabLutV[i] = 128;
-			else
-				lut->tabLutV[i] = lut->tabLutV[i]-nb;
-			
-			// couche des bleus
-			if((lut->tabLutB[i]-nb)>128)
-				lut->tabLutB[i] = 128;
-			else
-				lut->tabLutB[i] = lut->tabLutB[i]-nb;
+	if (new_lut != NULL) {
+		
+		initLUT(p_llut,new_lut);
+		int i;
+		
+		for(i=0;i<256;i++) {
+
+			if(new_lut->tabLutR[i] < 128) {
+				// couche des rouges
+				if((new_lut->tabLutR[i]-nb)>128)
+					new_lut->tabLutR[i] = 128;
+				else
+					new_lut->tabLutR[i] = new_lut->tabLutR[i]-nb;
+				
+				// couche des verts
+				if((new_lut->tabLutV[i]-nb)>128)
+					new_lut->tabLutV[i] = 128;
+				else
+					new_lut->tabLutV[i] = new_lut->tabLutV[i]-nb;
+				
+				// couche des bleus
+				if((new_lut->tabLutB[i]-nb)>128)
+					new_lut->tabLutB[i] = 128;
+				else
+					new_lut->tabLutB[i] = new_lut->tabLutB[i]-nb;
+			}
+				
+			else if (new_lut->tabLutR[i] >= 128) {
+				// rouge
+				if((new_lut->tabLutR[i]+nb)<128)
+					new_lut->tabLutR[i] = 128;
+				else
+					new_lut->tabLutR[i] = new_lut->tabLutR[i]+nb;
+				
+				// vert	
+				if((new_lut->tabLutV[i]+nb)<128)
+					new_lut->tabLutV[i] = 128;
+				else
+					new_lut->tabLutV[i] = new_lut->tabLutV[i]+nb;
+				
+				// bleu	
+				if((new_lut->tabLutB[i]+nb)<128)
+					new_lut->tabLutB[i] = 128;
+				else
+					new_lut->tabLutB[i] = new_lut->tabLutB[i]+nb;
+			}
 		}
-			
-		else if (lut->tabLutR[i] >= 128) {
-			// rouge
-			if((lut->tabLutR[i]+nb)<128)
-				lut->tabLutR[i] = 128;
-			else
-				lut->tabLutR[i] = lut->tabLutR[i]+nb;
-			
-			// vert	
-			if((lut->tabLutV[i]+nb)<128)
-				lut->tabLutV[i] = 128;
-			else
-				lut->tabLutV[i] = lut->tabLutV[i]+nb;
-			
-			// bleu	
-			if((lut->tabLutB[i]+nb)<128)
-				lut->tabLutB[i] = 128;
-			else
-				lut->tabLutB[i] = lut->tabLutB[i]+nb;
-		}
+	}
+	else {
+		printf("Erreur d'allocation de la lut \n");
 	}
 }	
 
 // pour diminuer le contrast on fait le processus inverse
 
-void dimContraste(LUT* lut, int nb) {
-	int i;
+void dimContraste(LLUT* p_llut, int nb) {
 
-	for(i=0;i<256;i++) {
+	LUT *new_lut = malloc(sizeof *new_lut);
 
-		if(lut->tabLutR[i] < 128) {
-			if((lut->tabLutR[i]+nb)>128)
-				lut->tabLutR[i] = 128;
-			else
-				lut->tabLutR[i] = lut->tabLutR[i]+nb;
+	if (new_lut != NULL) {
+		
+		initLUT(p_llut,new_lut);
+		int i;
+
+		for(i=0;i<256;i++) {
+
+			if(new_lut->tabLutR[i] < 128) {
+				if((new_lut->tabLutR[i]+nb)>128)
+					new_lut->tabLutR[i] = 128;
+				else
+					new_lut->tabLutR[i] = new_lut->tabLutR[i]+nb;
+					
+				if((new_lut->tabLutV[i]+nb)>128)
+					new_lut->tabLutV[i] = 128;
+				else
+					new_lut->tabLutV[i] = new_lut->tabLutV[i]+nb;
+					
+				if((new_lut->tabLutB[i]+nb)>128)
+					new_lut->tabLutB[i] = 128;
+				else
+					new_lut->tabLutB[i] = new_lut->tabLutB[i]+nb;
+			}
 				
-			if((lut->tabLutV[i]+nb)>128)
-				lut->tabLutV[i] = 128;
-			else
-				lut->tabLutV[i] = lut->tabLutV[i]+nb;
-				
-			if((lut->tabLutB[i]+nb)>128)
-				lut->tabLutB[i] = 128;
-			else
-				lut->tabLutB[i] = lut->tabLutB[i]+nb;
-		}
-			
-		else if (lut->tabLutR[i] >= 128) {
-			if((lut->tabLutR[i]-nb)<128)
-				lut->tabLutR[i] = 128;
-			else
-				lut->tabLutR[i] = lut->tabLutR[i]-nb;
-				
-			if((lut->tabLutV[i]-nb)<128)
-				lut->tabLutV[i] = 128;
-			else
-				lut->tabLutV[i] = lut->tabLutV[i]-nb;
-				
-			if((lut->tabLutB[i]-nb)<128)
-				lut->tabLutB[i] = 128;
-			else
-				lut->tabLutB[i] = lut->tabLutB[i]-nb;
+			else if (new_lut->tabLutR[i] >= 128) {
+				if((new_lut->tabLutR[i]-nb)<128)
+					new_lut->tabLutR[i] = 128;
+				else
+					new_lut->tabLutR[i] = new_lut->tabLutR[i]-nb;
+					
+				if((new_lut->tabLutV[i]-nb)<128)
+					new_lut->tabLutV[i] = 128;
+				else
+					new_lut->tabLutV[i] = new_lut->tabLutV[i]-nb;
+					
+				if((new_lut->tabLutB[i]-nb)<128)
+					new_lut->tabLutB[i] = 128;
+				else
+					new_lut->tabLutB[i] = new_lut->tabLutB[i]-nb;
+			}
 		}
 	}
+	else {
+		printf("Erreur d'allocation de la lut \n");
+	}
 }	
-
-
-
 
 
 
 
 //couleur en négatifs, inversion des valeur des pixel par rapport à la 
 //gamme de valeurs possible en RVB de 0 a 255
-void invert(LUT* lut) {	
-	int i;
-   	for (i=0; i<256; i++) {
-   		lut->tabLutR[i] = 255-lut->tabLutR[i];
-   		lut->tabLutV[i] = 255-lut->tabLutV[i];
-   		lut->tabLutB[i] = 255-lut->tabLutB[i];
-   	}
+void invert(LLUT* p_llut) {	
+	LUT *new_lut = malloc(sizeof *new_lut);
+
+	if (new_lut != NULL) {
+		
+		initLUT(p_llut,new_lut);
+		int i;
+	   	for (i=0; i<256; i++) {
+	   		new_lut->tabLutR[i] = 255-new_lut->tabLutR[i];
+	   		new_lut->tabLutV[i] = 255-new_lut->tabLutV[i];
+	   		new_lut->tabLutB[i] = 255-new_lut->tabLutB[i];
+	   	}
+	}
+	else {
+		printf("Erreur d'allocation de la lut \n");
+	}
 } 	
 
 /*Effet Sepia*/
@@ -230,30 +443,41 @@ void B_W(Image* img) {
 	}
 }
 
-void colorize(LUT* lut, int R, int V, int B) {
-	int i;
-	for (i=0; i<256; i++) {
+void colorize(LLUT* p_llut, int R, int V, int B) {
 
-		if((lut->tabLutR[i]+R)<0)
- 			lut->tabLutR[i] = 0;
- 		else if((lut->tabLutR[i]+R)>255)
- 			lut->tabLutR[i] = 255;
- 		else
- 			lut->tabLutR[i] = lut->tabLutR[i]+R;
- 			
- 		if((lut->tabLutV[i]+V)<0)
- 			lut->tabLutV[i] = 0;
- 		else if((lut->tabLutV[i]+V)>255)
- 			lut->tabLutV[i] = 255;
- 		else
- 			lut->tabLutV[i] = lut->tabLutV[i]+V;
- 			
- 		if((lut->tabLutB[i]+B)<0)
- 			lut->tabLutB[i] = 0;
- 		else if((lut->tabLutB[i]+B)>255)
- 			lut->tabLutB[i] = 255;
- 		else
-   			lut->tabLutB[i] = lut->tabLutB[i]+B;
+	LUT *new_lut = malloc(sizeof *new_lut);
+
+	if (new_lut != NULL) {
+		
+		initLUT(p_llut,new_lut);
+
+		int i;
+		for (i=0; i<256; i++) {
+
+			if((new_lut->tabLutR[i]+R)<0)
+	 			new_lut->tabLutR[i] = 0;
+	 		else if((new_lut->tabLutR[i]+R)>255)
+	 			new_lut->tabLutR[i] = 255;
+	 		else
+	 			new_lut->tabLutR[i] = new_lut->tabLutR[i]+R;
+	 			
+	 		if((new_lut->tabLutV[i]+V)<0)
+	 			new_lut->tabLutV[i] = 0;
+	 		else if((new_lut->tabLutV[i]+V)>255)
+	 			new_lut->tabLutV[i] = 255;
+	 		else
+	 			new_lut->tabLutV[i] = new_lut->tabLutV[i]+V;
+	 			
+	 		if((new_lut->tabLutB[i]+B)<0)
+	 			new_lut->tabLutB[i] = 0;
+	 		else if((new_lut->tabLutB[i]+B)>255)
+	 			new_lut->tabLutB[i] = 255;
+	 		else
+	   			new_lut->tabLutB[i] = new_lut->tabLutB[i]+B;
+		}
+	}
+	else {
+		printf("Erreur d'allocation de la lut \n");
 	}
 }
 
@@ -263,10 +487,9 @@ void sepia (LUT* lut, Image* img) {
 }
 
 
-/*
+
 int main(int argc, char const *argv[]) {
 
 	printf("ça compile \n");
 	return 0;
 }
-*/
